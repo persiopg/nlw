95f35dd58f97723d15463e802ef1090aab1940ea
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2023-05-16 17:52:18 -0300

    new file:   .eslintrc.json 	modified:   .gitignore   modified:   package-lock.json 	modified:   package.json 	modified:   src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
+
+app.get('/', () => {
+    return 'hello World'
+})
 
 app
-	.listen({
-		port: 3333,
-	})
-	.then(() => {
-		console.log("listening on port:3333");
-	});
+    .listen({
+        port: 3333,
+    })
+    .then(() => {
+        console.log('listening on port:3333')
+    })
+
+console.log('eu estive aqui')
